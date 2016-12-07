08b2d977dd45abfc70bead64d9cd91e85e51a6f1
Pascal <pascal.henrichon@hotmail.com>
2016-12-07 15:02:35 -0500

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,8 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \App\Http\Middleware\HttpsProtocol
+
     ];
 
     /**
@@ -30,6 +32,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
         ],
 
         'api' => [
