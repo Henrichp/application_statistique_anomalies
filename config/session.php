dd0ca12d7e994d770297379dace7a855f0f520eb
Pascal <pascal.henrichon@hotmail.com>
2016-12-08 21:28:16 -0500

    corrections

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 1200,
+    'lifetime' => 120,
 
-    'expire_on_close' => true,
+    'expire_on_close' => false,
 
     /*
     |--------------------------------------------------------------------------
